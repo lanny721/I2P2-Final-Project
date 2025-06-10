ed77d2a479662bdc93ddac80e6401c6640e2c132
xiiiiigua0421 <awesome20060421@gmail.com>
2025-06-11 01:50:56 +0800

    移除Player1.cpp多餘註解

--- Player/Player1.cpp
+++ Player/Player1.cpp
@@ -26,21 +26,15 @@ void Player::Update(float deltaTime) {
         Engine::GameEngine::GetInstance().keyStates[ALLEGRO_KEY_UP] || Engine::GameEngine::GetInstance().keyStates[ALLEGRO_KEY_LEFT] ||
         Engine::GameEngine::GetInstance().keyStates[ALLEGRO_KEY_RIGHT] || Engine::GameEngine::GetInstance().keyStates[ALLEGRO_KEY_DOWN]) {
             if        (Engine::GameEngine::GetInstance().keyStates[ALLEGRO_KEY_W] || Engine::GameEngine::GetInstance().keyStates[ALLEGRO_KEY_UP]) {
-                // position.y -= speed * deltaTime;
                 Engine::GameEngine::GetInstance().GetActiveScene()->camera.y -= speed * deltaTime;
                 // if (camera.y < 0) camera.y = 0;
             } else if (Engine::GameEngine::GetInstance().keyStates[ALLEGRO_KEY_S] || Engine::GameEngine::GetInstance().keyStates[ALLEGRO_KEY_DOWN]) {
-                // position.y += speed * deltaTime;
                 Engine::GameEngine::GetInstance().GetActiveScene()->camera.y += speed * deltaTime;
                 // if (camera.y > MapHeight * BlockSize - GetClientSize().y) camera.y = MapHeight * BlockSize - GetClientSize().y;
             } if (Engine::GameEngine::GetInstance().keyStates[ALLEGRO_KEY_A] || Engine::GameEngine::GetInstance().keyStates[ALLEGRO_KEY_LEFT]) {
-                // position.x -= speed * deltaTime;
                 Engine::GameEngine::GetInstance().GetActiveScene()->camera.x -= speed * deltaTime;
-                // if (camera.x < 0) camera.x = 0;
             } else if (Engine::GameEngine::GetInstance().keyStates[ALLEGRO_KEY_D] || Engine::GameEngine::GetInstance().keyStates[ALLEGRO_KEY_RIGHT]) {
-                // position.x += speed * deltaTime;
                 Engine::GameEngine::GetInstance().GetActiveScene()->camera.x += speed * deltaTime;
-                // if (camera.x > MapWidth * BlockSize - GetClientSize().x) camera.x = MapWidth * BlockSize - GetClientSize().x;
             }
             std::cout << "Position: (" << position.x << ", " << position.y << ")" << std::endl;
     }
