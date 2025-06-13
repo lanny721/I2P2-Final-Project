e581cef4b3851cecfcceb4a29f32916c9fa52c0e
sal825 <tn236125@gmail.com>
2025-06-14 00:40:21 +0800

    change the name to castle defense

--- Scene/StartScene.cpp
+++ Scene/StartScene.cpp
@@ -32,7 +32,7 @@ void StartScene::Initialize() {
     background->followCamera = false; // 背景固定在螢幕上
     AddNewObject(background); // 將背景加入場景，最底層渲染
 
-    AddNewObject(new Engine::Label("Tower Defense", "pirulen.ttf", 120, halfW, halfH / 3 + 50, 10, 255, 255, 255, 0.5, 0.5));
+    AddNewObject(new Engine::Label("Castle Defense", "pirulen.ttf", 120, halfW, halfH / 3 + 50, 10, 255, 255, 255, 0.5, 0.5));
 
     btn = new Engine::ImageButton("stage-select/dirt.png", "stage-select/floor.png", halfW - 200, halfH / 2 + 200, 400, 100);
     btn->SetOnClickCallback(std::bind(&StartScene::PlayOnClick, this, 1));
